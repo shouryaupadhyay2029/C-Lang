38dc8891a818994108db723323de8efd32006efd
Shourya Upadhyay <upadhyayshourya352@gmail.com>
2025-12-15 17:30:19 +0530

    fprintf

--- writeFILE.c
+++ writeFILE.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 int main(){
-    FILE *fp;
-    fp = fopen("filehandling.txt", "w");
-    printf("FILE HANDLING IN C\n",fp);
-    fclose(fp);
+    FILE *fptr;
+    fptr = fopen("filehandling.txt", "w");
+    fprintf(fptr,"%c",'m');
+    fprintf(fptr,"%c",'a');
+    fprintf(fptr,"%c",'n');
+    fprintf(fptr,"%c",'g');
+    fprintf(fptr,"%c",'o');
+    fclose(fptr);
     return 0;  
 
 }
